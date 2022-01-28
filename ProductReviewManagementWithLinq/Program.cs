e9dc7b83dadee8eb3ba9bbc80ef037b676337fcc
Your Name <akhilkumarchandragiri@gmail.com>
2022-01-28 22:37:40 +0530

    [Akhil] Master Program

---
+++ ProductReviewManagementWithLinq/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductReviewManagementWithLinq
+{
+    /// <summary>
+    /// Master branch of "Product Review Management"
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program of Product Review Management");
+        }
+    }
+}
