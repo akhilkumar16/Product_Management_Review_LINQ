0d4f9e0ffd3922d51bb90fafb97c3c1f1ddb3df4
Your Name <akhilkumarchandragiri@gmail.com>
2022-01-29 12:00:51 +0530

    [Akhil] Uc1 top 3 products

---
+++ ProductReviewManagementWithLinq/Management.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ProductReviewManagementWithLinq
+{
+    class Management
+    {
+        public void RetrieveTop3Records(List<ProductReview> review)
+        {
+            var recordData = (from products in review
+                              orderby products.Rating descending
+                              select products).Take(3);
+
+            foreach (var list in recordData)
+            {
+                Console.WriteLine("Product ID: " + list.ProductID + " " + "UserID: " + list.UserID + " " + "Rating: " + list.Rating + " " +
+                   " Review: " + list.Review + " " + "islike: " + list.isLike);
+            }
+        }
+    }
+}
